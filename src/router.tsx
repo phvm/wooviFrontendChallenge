59611d5358e15903ca9d8f1d39456f9af1886b6e
Pedro Moura <Pedrohvdm@gmail.com>
2024-07-10 17:24:38 -0300

    feat: added home page to redirect

--- src/router.tsx
+++ src/router.tsx
@@ -1,10 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 import PaymentMethod from "./pages/paymentMethod";
+import Links from "./utils/constants";
+import { Home } from "./pages/home";
+
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <div></div>
+    path: Links.home,
+    element: < Home />
   },
   {
     path: '/paymentMethod',
