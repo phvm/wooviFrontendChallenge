ddb3191047e67964413e7e710d536dbae45aded2
Pedro Moura <Pedrohvdm@gmail.com>
2024-07-10 17:26:17 -0300

    feat: ongoing payment method page

--- src/pages/paymentMethod/index.tsx
+++ src/pages/paymentMethod/index.tsx
@@ -1,5 +1,14 @@
+import LogoContainer from "../../components/logoContainer"
+import { getData } from "../../fakeAPI/getData"
+import { PageContainer, PaymentMethodText } from "./styles";
+
 export default function PaymentMethod(){
+  const data = getData();
+
   return (
-    <div>oi</div>
+    <PageContainer>
+      <LogoContainer />
+      <PaymentMethodText>{`${data.userInfo.name}, como você gostaria de pagar?`}</PaymentMethodText>
+    </PageContainer>
   )
 }
---
+++ src/pages/paymentMethod/styles.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const PageContainer = styled.main`
+  width: 100dvw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const PaymentMethodText = styled.p``;
