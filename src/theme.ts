5dc18ad2469915b3d9347f39ed484363039ac5fa
phvm <pedrohvdm@gmail.com>
2024-07-11 17:57:34 -0300

    fix: default font color

--- src/theme.ts
+++ src/theme.ts
@@ -2,6 +2,7 @@ import { createTheme } from "@mui/material";
 
 const theme = createTheme({
   typography: {
+    allVariants: { color: "#4D4D4D" },
     fontFamily: ["Nunito", "sans-serif"].join(","),
   },
 });
