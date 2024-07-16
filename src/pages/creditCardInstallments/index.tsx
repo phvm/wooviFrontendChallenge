828a8297d2711976cff4176f096ed9e4df93dad6
phvm <pedrohvdm@gmail.com>
2024-07-16 19:53:36 -0300

    feat: handle credit card payment

--- src/pages/creditCardInstallments/index.tsx
+++ src/pages/creditCardInstallments/index.tsx
@@ -10,11 +10,29 @@ import { PageContainer } from "./styles";
 import { UserContext } from "../../utils/contexts/UserContext";
 import { PaymentContext } from "../../utils/contexts/PaymentContext";
 import PaymentInfos from "../../components/paymentInfos";
+import ActionButton from "../../components/actionButton";
+import { toast } from "react-toastify";
 
 export default function CreditCardInstallments() {
   const { userInfo } = useContext(UserContext);
-  const { selectedPayment } = useContext(PaymentContext);
+  const {
+    selectedPayment,
+    paymentInstallments,
+    handleInstallmentContextChange,
+  } = useContext(PaymentContext);
 
+  function handleButtonClick() {
+    const payments = [...paymentInstallments];
+    const notPaidIndex = payments.findIndex(
+      (installment) => !installment.isPaid
+    );
+    if (payments[notPaidIndex]) {
+      payments[notPaidIndex].isPaid = true;
+      handleInstallmentContextChange(payments);
+      return toast.success("Pagamento realizado com sucesso");
+    }
+    return toast.error("Não há mais pagamentos a serem realizados");
+  }
   return (
     <>
       <header>
@@ -23,6 +41,7 @@ export default function CreditCardInstallments() {
       <PageContainer>
         <PageMessage>{`${userInfo.name.split(" ")[0]}, pague o restante em ${selectedPayment.installments - 1}x no cartão`}</PageMessage>
         <CreditCardForm userInfo={userInfo} />
+        <ActionButton content="Pagar" handleButtonClick={handleButtonClick} />
         <PaymentInfos
           paymentIdentifier={selectedPayment.identifier}
           paymentAmount={selectedPayment.amount}
